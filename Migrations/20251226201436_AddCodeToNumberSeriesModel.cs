8f7152098ca90adb982e1c8cd7c2b64d06f47160
hekmatbeigverdi <hb8info@gmail.com>
2025-12-30 23:50:57 +0330

    Add Code column to NumberSeries and account sub-ledger rules for party requirements

---
+++ Migrations/20251226201436_AddCodeToNumberSeriesModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LedgerCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCodeToNumberSeriesModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "NumberSeries",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "NumberSeries");
+        }
+    }
+}
---
+++ Migrations/20251228203954_AddAccountSubLedgerRules.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LedgerCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountSubLedgerRules : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AllowedPartyType",
+                table: "Accounts",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RequiresParty",
+                table: "Accounts",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowedPartyType",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "RequiresParty",
+                table: "Accounts");
+        }
+    }
+}
