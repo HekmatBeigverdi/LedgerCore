5d53299c72ca9f3118fd2065820ed59fbc920ffe
hekmatbeigverdi <hb8info@gmail.com>
2026-02-25 18:13:24 +0330

    Add RowVersion column to NumberSeries for concurrency handling

---
+++ Migrations/20260224220050_AddRowVersionToNumberSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LedgerCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowVersionToNumberSeries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RowVersion",
+                table: "NumberSeries",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: false)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "NumberSeries");
+        }
+    }
+}
