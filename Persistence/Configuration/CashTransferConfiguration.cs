e2f00e95c5c76d6258b7644056d5be4b8f6b252c
hekmatbeigverdi <hb8info@gmail.com>
2026-03-12 11:59:30 +0330

    Enhance CashTransferConfiguration to include FromAccount and ToAccount relationships for improved account tracking

--- Persistence/Configuration/CashTransferConfiguration.cs
+++ Persistence/Configuration/CashTransferConfiguration.cs
@@ -20,8 +20,19 @@ public class CashTransferConfiguration : IEntityTypeConfiguration<CashTransfer>
             .WithMany()
             .HasForeignKey(x => x.BranchId)
             .OnDelete(DeleteBehavior.Restrict);
+        builder.HasOne(x => x.FromAccount)
+            .WithMany()
+            .HasForeignKey(x => x.FromAccountId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.ToAccount)
+            .WithMany()
+            .HasForeignKey(x => x.ToAccountId)
+            .OnDelete(DeleteBehavior.Restrict);
 
         builder.HasIndex(x => new { x.BranchId, x.Number }).IsUnique();
+        builder.Property(x => x.FromAccountId).IsRequired();
+        builder.Property(x => x.ToAccountId).IsRequired();
         
         builder.HasIndex(x => x.Date);
     }
