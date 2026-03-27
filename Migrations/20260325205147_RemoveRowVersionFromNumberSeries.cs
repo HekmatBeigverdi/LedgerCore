5bddad73c8f6c7080bbaadb6612db8333232f72b
hekmatbeigverdi <hb8info@gmail.com>
2026-03-27 09:22:02 +0330

    Update migration for remove RowVersion from NumberSeries

---
+++ Migrations/20260325205147_RemoveRowVersionFromNumberSeries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LedgerCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRowVersionFromNumberSeries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "NumberSeries");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RowVersion",
+                table: "NumberSeries",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
