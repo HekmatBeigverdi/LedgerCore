d2362d30eed6d8eeae1e21305bf668f082e2ef32
hekmatbeigverdi <hb8info@gmail.com>
2026-03-29 08:41:41 +0330

    Add ProjectDto for project management

---
+++ Core/Models/Master/ProjectDto.cs
@@ -0,0 +1,12 @@
+namespace LedgerCore.Core.Models.Master;
+
+public class ProjectDto
+{
+    public int Id { get; set; }
+    public string Code { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string? Description { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public bool IsActive { get; set; }
+}
